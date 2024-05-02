13f2b6724c51b181ae2defae2540009b211728eb
nangtoferia <nangtoferia@gmail.com>
2024-05-02 23:42:12 +0700

    Update video-card.tsx

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -14,7 +14,7 @@ const VideoCard = ({ video }: any) => {
         '@context': 'https://schema.org',
         '@type': 'Article',
         headline: `${video.title} - ${SITENAME}`,
-        image: video.splash_img,
+        image: video.single_img,
         description: `${video.title} di ${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma`,
         url: `https://bokephd.pages.dev/v/${video.file_code}`
         }
